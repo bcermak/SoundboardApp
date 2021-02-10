e3de640591162415ab42f3caeb2eafd2e3a9177e
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-10 16:18:05 -0600

    refactored the play music functions

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
 
-
+// Turns music on or off based on clicks
     pads.forEach((pad, index) => {
         pad.addEventListener("click", function() {
             sounds[index].currentTime = 0;
           sounds[index].play();
+        pad.addEventListener("dblclick", function() {
+            console.log("Double");
+            sounds[index].pause();
+    
+        })
         
         });
       });
-
-      pads.forEach((pad, index) => {
-        pad.addEventListener("dblclick", function() {
-        console.log("Double");
-        sounds[index].pause();
-
-    })
-    })
